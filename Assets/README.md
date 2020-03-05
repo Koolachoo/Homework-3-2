58d8183fb58ef20af36afa040afa9a35a947039d
Justin Taylor <kiradez211@gmail.com>
2020-03-05 01:02:44 -0500

    Finals changes

---
+++ Assets/README.md
@@ -0,0 +1,5 @@
+## URL ##
+https://koolachoo.github.io/Homework-3-2/
+
+## How it Works ##
+This is a weather search that is supposed to display the current forcast, along with the 5 day forecast. Within each, the 
--- Assets/script.js
+++ Assets/script.js
@@ -60,21 +60,25 @@ $("#searchBtn").on("click", function () {
     $("#searchTerm").val("");
     // full url to call api  const 
     queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
-    // var lati 
-    // var long
+   
 
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
+        console.log(response);
         let tempF = (response.main.temp - 273.15) * 1.80 + 32; console.log(Math.floor(tempF))
         getCurrentConditions(response); getCurrentForecast(response); makeList();
     })
 });
+
+// var lati 
+// var long
 // queryUrl2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKeyS + "&lat=" + lati + "&lon=" +long;
 // $.ajax({ url: queryUrl2, method: "GET" }).then(function (response2) {
-//   console.log(response2)
-//     // getCurrentConditions(response); getCurrentForecast(response); makeList();
+//   console.log(response2);
+//    lati = response.coord.lat;
+//    long = response.coord.lon;
 // })
 
 // $(".card").on("click", function(response) {
@@ -82,11 +86,11 @@ $("#searchBtn").on("click", function () {
 //          });
 function makeList() { let listItem = $("<li>").addClass("list-group-item").text(city); $(".list").append(listItem); }
 function getCurrentConditions(response) {
-    // get the temperature and convert to fahrenheit     
+
     tempF = (response.main.temp - 273.15) * 1.80 + 32;
     tempF = Math.floor(tempF);
     $('#currentCity').empty();
-    // get and set the content     
+    
     const card = $("<div>").addClass("card");
     const cardBody = $("<div>").addClass("card-body");
     const city = $("<h4>").addClass("card-title").text(response.name);
@@ -95,8 +99,8 @@ function getCurrentConditions(response) {
     const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
     const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
     const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-    //    UV = $("<p>").addClass("card-text current-UV").text("UV Index: " + response)
-    // add to page   
+    //  const UV = $("<p>").addClass("card-text current-UV").text("UV Index: " + response)
+   
     city.append(cityDate, image)
     cardBody.append(city, temperature, humidity, wind);
     card.append(cardBody);
@@ -107,17 +111,17 @@ function getCurrentForecast() {
         console.log(response)
         console.log(response.dt)
         $('#forecast').empty();
-        // variable to hold response.list    
+     
         let results = response.list;
-        console.log(results)        //declare start date to check against    // 
-        startDate = 20    //have end date, 
+        console.log(results)    
+        startDate = 20    
         endDate = startDate + 5
         for (let i = 0; i < results.length; i++) {
             let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
             let hour = results[i].dt_txt.split('-')[2].split(' ')[1]; console.log(day); console.log(hour);
-            if (results[i].dt_txt.indexOf("12:00:00") !== -1) {                // get the temperature and convert to fahrenheit         
+            if (results[i].dt_txt.indexOf("12:00:00") !== -1) {                     
                 let temp = (results[i].main.temp - 273.15) * 1.80 + 32; let tempF = Math.floor(temp);
-                const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+                const card = $("<div>").addClass("card col-md-2 ml-4 bg-secondary text-white");
                 const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
                 const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
                 const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
